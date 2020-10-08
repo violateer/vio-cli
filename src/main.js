f1161bf6add6c8bb817fbff989205e2001447895
violateer <1828257089@qq.com>
2020-10-08 13:24:26 +0800

    配置指令命令

--- src/main.js
+++ src/main.js
@@ -1,8 +1,51 @@
 const {
     version
 } = require('./constantes')
+const program = require('commander');
+// 配置3个指令命令
+const mapActions = {
+    create: {
+        alias: "c",
+        description: "create a project",
+        examples: ["vio-cli create <project-name>"]
+    },
+    config: {
+        alias: "conf",
+        description: "config project variable",
+        examples: ["vio-cli config set <k><v>", "vio-cli config get <k>"]
+    },
+    "*": {
+        alias: "",
+        description: "command not found",
+        examples: []
+    }
+}
+
+// 循环创建命令
+Reflect.ownKeys(mapActions).forEach(action => {
+    program
+        .command(action) // 配置命令的名称
+        .alias(mapActions[action].alias) // 配置命令的别称
+        .description(mapActions[action].description) // 配置命令的描述
+        .action(() => {
+            if (action === '*') {
+                // 访问不到对应的命令
+                console.log(mapActions[action].description);
+            } else {
+                console.log(action);
+            }
+        })
+})
+
+// 监听用户的--help事件
+program.on('--help', () => {
+    console.log('\nExamples:');
+    Reflect.ownKeys(mapActions).forEach(action => {
+        mapActions[action].examples.forEach(example => {
+            console.log(example);
+        })
+    })
+})
 
 // 获取版本号
-const program = require('commander');
-
 program.version(version).parse(process.argv);
